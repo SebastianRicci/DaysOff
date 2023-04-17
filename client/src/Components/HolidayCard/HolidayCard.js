ba2fd97cd2ce61b050580a9fdf07595c191f4f9d
SebastianRicci <sebastianricciz@gmail.com>
2023-04-17 15:24:05 -0400

    Holiday card props

--- client/src/Components/HolidayCard/HolidayCard.js
+++ client/src/Components/HolidayCard/HolidayCard.js
@@ -1,12 +1,12 @@
 import "./HolidayCard.css";
 
-export default function HolidayCard() {
+export default function HolidayCard({ Name, Date }) {
   return (
     <div className="cardContainer">
       <h1 className="cardDate">23</h1>
       <div className="cardDescription">
-        <div className="HolidayName">Christmas</div>
-        <div className="HolidayDate">December 25th</div>
+        <div className="HolidayName">{Name}</div>
+        <div className="HolidayDate">{Date} 25th</div>
       </div>
     </div>
   );
