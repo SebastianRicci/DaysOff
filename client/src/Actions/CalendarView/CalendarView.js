94b53a2ac6677860bb365a212957520da2658f8a
SebastianRicci <sebastianricciz@gmail.com>
2023-04-21 09:05:50 -0400

    Action button moved to only calendar view

--- client/src/Actions/CalendarView/CalendarView.js
+++ client/src/Actions/CalendarView/CalendarView.js
@@ -1,15 +1,30 @@
 import "./CalendarView.css";
-
 import Holidays from "../../Components/Holidays/Holidays";
 import MonthCalendar from "../../Components/MonthCalendar/MonthCalendar";
 import CalendarOverview from "../../Components/CalendarOverview/CalendarOverview";
+import Fab from "@mui/material/Fab";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 
 export default function CalendarView() {
+  const style = {
+    background: "#fd1079",
+    color: "white",
+    margin: 0,
+    top: "auto",
+    right: 20,
+    bottom: 80,
+    left: "auto",
+    position: "fixed",
+  };
   return (
     <div className="CalendarContainer">
       <MonthCalendar></MonthCalendar>
       <Holidays></Holidays>
       <CalendarOverview></CalendarOverview>
+      <Fab variant="extended" style={style}>
+        <CalendarMonthIcon sx={{ mr: 1 }} />
+        Optimize
+      </Fab>
     </div>
   );
 }
--- client/src/Components/BottomNav/BottomNav.js
+++ client/src/Components/BottomNav/BottomNav.js
@@ -4,28 +4,13 @@ import Box from "@mui/material/Box";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import Paper from "@mui/material/Paper";
-import Fab from "@mui/material/Fab";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import ModeOfTravelIcon from "@mui/icons-material/ModeOfTravel";
 import BarChartIcon from "@mui/icons-material/BarChart";
 
 export default function BottomNav({ action, setAction }) {
-  const style = {
-    background: "#fd1079",
-    color: "white",
-    margin: 0,
-    top: "auto",
-    right: 20,
-    bottom: 80,
-    left: "auto",
-    position: "fixed",
-  };
   return (
     <Box sx={{ width: 400 }}>
-      <Fab variant="extended" style={style}>
-        <CalendarMonthIcon sx={{ mr: 1 }} />
-        Optimize
-      </Fab>
       <Paper
         sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
         elevation={3}
