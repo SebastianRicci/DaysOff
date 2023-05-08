152ea31ee199f5848314c28fe40080010655960f
SebastianRicci <sebastianricciz@gmail.com>
2023-05-08 17:21:13 -0400

    Added vercel analytics

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard/Dashboard";
+import { inject } from "@vercel/analytics";
 
 function App() {
+  inject();
   return (
     <BrowserRouter>
       <Routes>
